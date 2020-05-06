28e481efec3376f25ff25025b1bfd040947e5345
hazle09 <joohyenim@gmail.com>
2020-05-06 12:50:24 +0900

    BookDAO

--- src/lecture1/midterm2/BookDAO.java
+++ src/lecture1/midterm2/BookDAO.java
@@ -10,21 +10,21 @@ public class BookDAO {
 	
 
 	    public static List<Book> findAll() throws Exception {
-	        String sql = "SELECT s.*, d.departmentName " +
-	                     "FROM student s LEFT JOIN department d ON s.departmentId = d.id";
+	        String sql = "SELECT b.*, c.categoryName " +
+	                     "FROM book b LEFT JOIN category c ON b.categoryId = c.id";
 	        try (Connection connection = DB.getConnection("book");
 	             PreparedStatement statement = connection.prepareStatement(sql);
 	             ResultSet resultSet = statement.executeQuery()) {
 	            ArrayList<Book> list = new ArrayList<Book>();
 	            while (resultSet.next()) {
-	                Book.student = new Student();
-	                Book.setId(resultSet.getInt("id"));
-	                Book.setStudentNumber(resultSet.getString("studentNumber"));
-	                Book.setName(resultSet.getString("name"));
-	                Book.setDepartmentId(resultSet.getInt("departmentId"));
-	                Book.setYear(resultSet.getInt("year"));
-	                Book.setDepartmentName(resultSet.getString("departmentName"));
-	                list.add(student);
+	                Book book = new Book();
+	                book.setId(resultSet.getInt("id"));
+	                book.setTitle(resultSet.getString("Title"));
+	                book.setCategoryId(resultSet.getInt("categoryId"));
+	                book.setPrice(resultSet.getInt("price"));
+	                book.setPublisher(resultSet.getString("publisher"));
+	                book.setCategoryName(resultSet.getString("categoryName"));
+	                list.add(book);
 	            }
 	            return list;
 	        }
@@ -33,4 +33,4 @@ public class BookDAO {
 
 
 
-}
+
