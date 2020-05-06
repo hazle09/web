7c3a7456775e56acddced60f43f1b7a3d44ed97b
hazle09 <joohyenim@gmail.com>
2020-05-06 12:36:55 +0900

    book

--- src/lecture1/midterm2/Book.java
+++ src/lecture1/midterm2/Book.java
@@ -1,5 +1,64 @@
 package lecture1.midterm2;
 
 public class Book {
+		int id;
+		String title;
+		String author;
+		int categoryId;
+		int price;
+		String publisher;
+		String categoryName;
+		boolean enabled;
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		public String getAuthor() {
+			return author;
+		}
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+		public int getCategoryId() {
+			return categoryId;
+		}
+		public void setCategoryId(int categoryId) {
+			this.categoryId = categoryId;
+		}
+		public int getPrice() {
+			return price;
+		}
+		public void setPrice(int price) {
+			this.price = price;
+		}
+		public String getPublisher() {
+			return publisher;
+		}
+		public void setPublisher(String publisher) {
+			this.publisher = publisher;
+		}
+		public String getCategoryName() {
+			return categoryName;
+		}
+		public void setCategoryName(String categoryName) {
+			this.categoryName = categoryName;
+		}
+		public boolean isEnabled() {
+			return enabled;
+		}
+		public void setEnabled(boolean enabled) {
+			this.enabled = enabled;
+		}
+		
+	}
 
-}
+
+
