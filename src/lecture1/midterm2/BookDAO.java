f36b3138d16b8bf880a5dc839eb369d7525f4844
hazle09 <joohyenim@gmail.com>
2020-05-06 13:14:35 +0900

    DAO

--- src/lecture1/midterm2/BookDAO.java
+++ src/lecture1/midterm2/BookDAO.java
@@ -32,13 +32,13 @@ public class BookDAO {
 	            return list;
 	        }
 	    }
-	    public static List<Book> findByName(String name) throws Exception {
+	    public static List<Book> findByName(String author) throws Exception {
 	        String sql = "SELECT b.*, c.categoryName " +
                     "FROM book b LEFT JOIN category c ON b.categoryId = c.id"+
-	                     "WHERE b.name LIKE ?";
+	                     "WHERE b.author LIKE ?";
 	        try (Connection connection = DB.getConnection("book");
 	             PreparedStatement statement = connection.prepareStatement(sql)) {
-	            statement.setString(1, name + "%");
+	            statement.setString(1, author + "%");
 	            try (ResultSet resultSet = statement.executeQuery()) {
 	                ArrayList<Book> list = new ArrayList<Book>();
 	                while (resultSet.next()) {
